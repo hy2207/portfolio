de0b10decf08b0492a12d31e95a2f5637ff91663
Hwayoung Choi <55370060+hy2207@users.noreply.github.com>
2023-05-22 22:29:34 +0900

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-f08elx*yf4=$+_jgk0udh^rk7_a8$x9i&1qp6131d5ntdwx8o7
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    "localhost",
+#     "localhost",
     "ec2-15-164-215-134.ap-northeast-2.compute.amazonaws.com",
     "www.hwayoungchoi.com",
     ".hwayoungchoi.com",
